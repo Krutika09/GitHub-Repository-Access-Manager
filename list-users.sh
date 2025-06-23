f690975d59f893fc45cfad96abbd64c0c6f71fdf
Krutika Yawale <148604628+Krutika09@users.noreply.github.com>
2025-06-23 08:36:29 +0530

    Update list-users.sh

--- list-users.sh
+++ list-users.sh
@@ -17,17 +17,18 @@ function github_api_get {
     local endpoint="$1"
     local url="${API_URL}/${endpoint}"
     curl -s -u "${USERNAME}:${TOKEN}" -H "Accept: application/vnd.github.v3+json" "$url"
-} # this function build fill github api url using endpoint and send get req using curl and authenticate using USERNAME and TOKEN
+} 
 
 # Function to list users with read access
 function list_users_with_read_access { 
-    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators" # Create endpoint get the list of collaborators from specific repo
-    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"  # Filter username using jq who has pull access means read access 
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators" 
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"  
     
-    if [[ -z "$collaborators" ]]; then  # if collaborator varible is empty then print msg "no user found " 
+    if [[ -z "$collaborators" ]]; then 
+    
         echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
     else
-        echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:" # if found then print username of collaborators. 
+        echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:" 
         echo "$collaborators"
     fi
 }
